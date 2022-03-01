8c3034a75bf15b8ea05e4da95079d4f1160c614d
BT_1E10_24 <sample@bths.edu>
2022-03-01 12:27:57 -0500

    Initial commit

---
+++ src/Address.java
@@ -0,0 +1,10 @@
+public class Address {
+    private String streetName;
+    private String streetNumber;
+    private String ApartmentNumber;
+    private String city;
+    private String state;
+    private int zipCode;
+
+    public Address()
+}
