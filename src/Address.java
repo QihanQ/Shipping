edb46293393f7fe05226d5b97be575070b3dc6ba
BT_1E10_24 <sample@bths.edu>
2022-03-02 12:30:18 -0500

    a

--- src/Address.java
+++ src/Address.java
@@ -1,10 +1,45 @@
 public class Address {
     private String streetName;
     private String streetNumber;
-    private String ApartmentNumber;
+    private String apartmentNumber;
     private String city;
     private String state;
     private int zipCode;
 
-    public Address()
+    public Address(Address a)
+    {
+        streetName = a.getStreetName();
+        streetNumber = a.getStreetNumber();
+    }
+
+    public String getStreetName()
+    {
+        return streetName;
+    }
+
+    public String getStreetNumber()
+    {
+        return streetNumber;
+    }
+
+    public String getApartmentNumber()
+    {
+        return apartmentNumber;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public String getState()
+    {
+        return state;
+    }
+
+    public int getZipCode()
+    {
+        return zipCode;
+    }
 }
+
