2dd1b3ac8614ec1e9c3079d74343cdde4be2e2f3
QihanQ <98603740+QihanQ@users.noreply.github.com>
2022-03-03 12:30:53 -0500

    Update Address.java

--- src/Address.java
+++ src/Address.java
@@ -5,11 +5,44 @@ public class Address {
     private String city;
     private String state;
     private int zipCode;
-
+    
+    public Address(String sNa, String sNu, String aN, String c, String s, int z)
+    {
+        streetName = sNa;
+        streetNumber = sNu;
+        apartmentNumber = aN;
+        city = c;
+        state = s;
+        zipCode = z;
+    }
+    
     public Address(Address a)
     {
         streetName = a.getStreetName();
         streetNumber = a.getStreetNumber();
+        apartmentNumber = a.getApartmentNumber();
+        city = a.getCity();
+        state = a.getState();
+        zipCode = a.getZipCode();
+    }
+    
+    public Address(String s)
+    {
+        streetNumber = s.substring(0 , s.indexOf(" ");
+        s = s.substring(s.indexOf(" ") + 1);
+        if(s.indexOf("Apt") != -1)
+        {
+            streetName = s.substring(0 , s.indexOf("Apt));
+            s = s.substring(s.indexOf("Apt"));
+            apartmentNumber = s.substring(s.indexOf(" ") + 1 , s.indexOf(","));
+            s = s.substring(s.indexOf(",") + 1);
+        }
+        else
+        {
+            streetName = s.substring(0 , s.indexOf(","));
+            s = s.substring(s.indexOf(",") + 1);
+        }
+        
     }
 
     public String getStreetName()
